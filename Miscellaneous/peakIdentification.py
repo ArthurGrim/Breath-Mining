ff18fdea3a4ca6d109a1c0a0d66c904605d25857
Strontium582 <Arthur582@hotmail.fr>
2019-11-29 08:54:53 +0100

    peakalignement clustering, peax software

--- Miscellaneous/peakIdentification.py
+++ Miscellaneous/peakIdentification.py
@@ -1,33 +1,32 @@
 import os
 from tkinter import Tk
-from tkinter.filedialog import askopenfilename
-from tkinter.filedialog import asksaveasfile
+import tkinter.filedialog as fd
 
 
 
 
-
-def peaxCommandMaker(inputFile, outputFile, paramFile):
-    inputFile = inputFile.split("/")[-1]
-    paramFile = paramFile.split("/")[-1]
-    os.popen("chmod 775 " + paramFile) #allow file to be read/open
-    return "./peax " + inputFile + " " + outputFile +" ./-p " + paramFile + "/"
+print(os.getcwd())
+def peaxCommandMaker(inputDir, outputDir, paramFile):
+    for inputFile in sorted(os.listdir(inputDir)):
+        if os.path.isfile(os.path.join(inputDir, inputFile)):
+            if "lock" not in inputFile:
+                # inputFile = inputFile.split("/")[-1]
+                paramFile = paramFile.split("/")[-1]
+                os.popen("chmod 775 " + paramFile) #allow file to be read/open
+                cmd = "./peax "+ inputDir.replace(os.getcwd()+"/","")+"/" + inputFile + " " +  outputDir.replace(os.getcwd()+"/","") + "/" + inputFile +" ./-p " + paramFile + "/"
+                print(cmd)
+                f = os.popen(cmd)
+                now = f.read()
+                print ("Results from peak Identification:", now)
 
 
 
 
 Tk().withdraw()
-ipF = askopenfilename()
-opF = "outputPeaks.txt"
-paF = askopenfilename()
+ipD = fd.askdirectory()
+opD = fd.askdirectory()
+paF = fd.askopenfilename()
 
 
 
-cmd = peaxCommandMaker(ipF, opF, paF)
-print(cmd)
-
-
-f = os.popen(cmd)
-now = f.read()
-
-print ("Results from peak Identification:", now)
+peaxCommandMaker(ipD, opD, paF)
